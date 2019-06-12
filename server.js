c57fae8fea9c63e67139b0c598445addab123bb0
Jack Polo <jackpolo@Jacks-MBP.fios-router.home>
2019-06-12 15:39:29 -0400

    ff

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+var port = process.env.PORT || 8080;
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json)
+app.listen(port,function(){
+    console.log ("app listening on port ",port)
+})
